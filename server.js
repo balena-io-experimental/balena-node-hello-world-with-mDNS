f6c8603ba88301a09573168f39624c7b2123f175
fisehara <fisehara@gmail.com>
2021-06-03 10:32:58 +0200

    Install mdns service inside the docker. Utilise node DNS resolve to test dns

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+
+const dns = require('dns');
+
+dns.lookup('google.com', (err, address, family) => {
+  console.log('Testing it: address: %j family: IPv%s', address, family);
+});
+
+
 // Enable HTML template middleware
 app.engine('html', require('ejs').renderFile);
 
@@ -11,6 +19,16 @@ app.use(express.static('styles'));
 app.get('/', function (req, res) {
   res.render('index.html');
 });
+
+app.get('/dnslookup/:name', function (req, res) {
+
+  dns.lookup(req.params.name, (err, address, family) => {
+    console.log('address: %j family: IPv%s', address, family);
+    res.json({ ip: address, type: family });
+  });
+});
+
+
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
